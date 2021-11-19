e8a91e3dc3a9cba59312ca35592b0145d6a0c8fa
nedahmadi <nedahmadi83@gmail.com>
2021-11-19 16:01:23 +0330

    edited css

--- java.js
+++ java.js
@@ -54,6 +54,7 @@ function displayForecast(response) {
              <div class="weather-forecast-temperature">
                <span class="weather-forecast-temperature-max">
                ${Math.round(forecastDay.temp.max)}°
+               |
                  </span>
                  <span class="weather-forecast-temperature-min">
                   ${Math.round(forecastDay.temp.min)}°
